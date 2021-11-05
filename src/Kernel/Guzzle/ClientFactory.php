05f5140c18383e15535371543ed1ba72cbcb0f72
青皮子 <1712715552@qq.com>
2021-11-05 23:11:45 +0800

    fix

--- src/Kernel/Guzzle/ClientFactory.php
+++ src/Kernel/Guzzle/ClientFactory.php
@@ -16,7 +16,6 @@ use Hyperf\Contract\ConfigInterface;
 use Hyperf\Guzzle\RetryMiddleware;
 use Psr\Container\ContainerInterface;
 use Hyperf\Logger\LoggerFactory;
-use Hyperf\Guzzle\HandlerStackFactory;
 use Qingpizi\HyperfFramework\Constants\Time;
 
 class ClientFactory
@@ -34,22 +33,9 @@ class ClientFactory
     public function create(array $options = []): Client
     {
         $config = $this->container->get(ConfigInterface::class);
-        $retryCount = $config->get('http.retry.count', 0);
-        if ($retryCount > 0) {
-            $middlewares = ['retry' => [RetryMiddleware::class, [$retryCount, $config->get('guzzle.retry.delay', 3 * Time::SECOND)]]];
-        }
-
-        if ($config->get('logger.default.custom.http.enable') !== false) {
-            $middlewares['logger'] = [HttpRequestAccessLogMiddleware::class, [$this->container->get(LoggerFactory::class), $config->get('logger.default.custom.http.timeout')]];
-        }
 
         $factory = new HandlerStackFactory();
-        $stack = $factory->create([
-            'min_connections' => $config->get('http.pool.min_connections', 1),
-            'max_connections' => $config->get('http.pool.max_connections', 30),
-            'wait_timeout' => $config->get('http.pool.wait_timeout', 3.0),
-            'max_idle_time' => $config->get('http.pool.max_idle_time', 60),
-        ], $middlewares);
+        $stack = $factory->create($config);
 
         $config = array_replace([
             'handler' => $stack,
---
+++ src/Kernel/Guzzle/HandlerStackFactory.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Qingpizi\HyperfFramework\Kernel\Guzzle;
+
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Guzzle\RetryMiddleware;
+use Hyperf\Logger\LoggerFactory;
+use Psr\Container\ContainerInterface;
+use Qingpizi\HyperfFramework\Constants\Time;
+
+class HandlerStackFactory
+{
+
+    public function create(ConfigInterface $config)
+    {
+        $retryCount = $config->get('http.retry.count', 0);
+        if ($retryCount > 0) {
+            $middlewares = ['retry' => [RetryMiddleware::class, [$retryCount, $config->get('guzzle.retry.delay', 3 * Time::SECOND)]]];
+        }
+
+        if ($config->get('logger.default.custom.http.enable') !== false) {
+            $middlewares['logger'] = [HttpRequestAccessLogMiddleware::class, [di()->get(LoggerFactory::class), $config->get('logger.default.custom.http.timeout')]];
+        }
+
+        $factory = new \Hyperf\Guzzle\HandlerStackFactory();
+        return $factory->create([
+            'min_connections' => $config->get('http.pool.min_connections', 1),
+            'max_connections' => $config->get('http.pool.max_connections', 30),
+            'wait_timeout' => $config->get('http.pool.wait_timeout', 3.0),
+            'max_idle_time' => $config->get('http.pool.max_idle_time', 60),
+        ], $middlewares);
+    }
+}
