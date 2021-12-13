807e69788d6ba61e320a88bc9dec531931e4635c
青皮子 <1712715552@qq.com>
2021-12-13 14:25:56 +0800

    fix 修改签名方式

--- src/Helper/SignHelper.php
+++ src/Helper/SignHelper.php
@@ -7,29 +7,47 @@ namespace Qingpizi\HyperfFramework\Helper;
 class SignHelper
 {
     /**
+     * 生产签名
      * @param array $data
      * @param string $key
      * @return string
      */
-    public static function makeSign(array $data, $key = ''): string
+    public static function makeSign(array $data, string $key = ''): string
     {
+        $data = self::filteringParams($data);
         ksort($data);
         $queryString = http_build_query($data);
-        return base64_encode(hash_hmac('sha256', $queryString, $key, true));
+        return md5($queryString. '-' . $key);
     }
 
     /**
+     * 验证签名
      * @param string $sign
      * @param array $data
      * @param string $key
      * @return bool
      */
-    public static function verifySign(string $sign, array $data, $key = ''): bool
+    public static function verifySign(string $sign, array $data, string $key = ''): bool
     {
+        $data = self::filteringParams($data);
         ksort($data);
         $queryString = http_build_query($data);
-        $realSign = base64_encode(hash_hmac('sha256', $queryString, $key, true));
+        $realSign = md5($queryString. '-' . $key);
+        return strcmp($sign, $realSign) === 0;
+    }
 
-        return strtolower($sign) === strtolower($realSign);
+    /**
+     * 过滤参数
+     * @param array $data
+     * @return array
+     */
+    private static function filteringParams(array $data): array
+    {
+        foreach ($data as $key => $value) {
+            if ($value === '' || is_null($value) ) {
+                unset($data[$key]);
+            }
+        }
+        return $data;
     }
 }
